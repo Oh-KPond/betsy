11ac0eff2832df4456f4121c65b1b1d2a8559e7d
Brittany Jones <brittany.r.jones@hotmail.com>
2018-04-19 10:09:10 -0700

    created user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419170403) do
+ActiveRecord::Schema.define(version: 20180419170846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "status"
+    t.integer "cc_num"
+    t.integer "cvv"
+    t.string "email"
+    t.string "street_address"
+    t.string "state"
+    t.string "city"
+    t.integer "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -29,6 +42,15 @@ ActiveRecord::Schema.define(version: 20180419170403) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.integer "uid"
+    t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
