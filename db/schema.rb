025729f4a79f4e9a40a173cf2827ae69f7efd12e
Brittany Jones <brittany.r.jones@hotmail.com>
2018-04-19 13:57:58 -0700

    removed extra orderproducts table

--- db/schema.rb
+++ db/schema.rb
@@ -50,11 +50,6 @@ ActiveRecord::Schema.define(version: 20180419202140) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "products_orders", force: :cascade do |t|
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
