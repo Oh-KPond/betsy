32807a789e4ddbe8ba20a345f448ae1d28836041
Lily Sky <lilyxsky@gmail.com>
2018-04-19 17:25:23 -0700

    tests begun

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,39 @@
 class ProductsController < ApplicationController
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    end
   end
 
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find_by(id: params[:id])
   end
 
   def edit
+    @product = Product.find_by(id: params[:id])
   end
 
   def update
   end
 
-  def destroy
+  def destroy #remove from actively available
+    @product = Product.find_by(id: params[:id])
+    if @product
+      @product.status = false
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :stock, :price, :description, :status, :user_id, :image_url)
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,39 +1,73 @@
 require "test_helper"
+require "pry"
 
 describe ProductsController do
-  it "should get new" do
-    get products_new_url
-    value(response).must_be :success?
+  describe "index" do
+    it "should get index" do
+        Product.count.must_equal 3
+        get products_path
+        must_respond_with :success
+    end
+
+    it "succeeds when there are no works" do
+        Product.all.each do |item|
+          item.destroy
+        end
+
+        Product.count.must_equal 0
+        get products_path
+        must_respond_with :success
+      end
   end
 
-  it "should get create" do
-    get products_create_url
-    value(response).must_be :success?
+  describe "new" do
+    it "succeeds" do
+      get product_path(products(:cat))
+      must_respond_with :success
+    end
   end
 
-  it "should get index" do
-    get products_index_url
-    value(response).must_be :success?
+  describe "create" do
+    # it "creates a product with valid data" do
+    #   proc {
+    #     post products_path, params: {
+    #       product: {
+    #         name: "Bunny",
+    #         stock: 14,
+    #         price: 5,
+    #         description: "Too many, please help",
+    #         status: true,
+    #         user_id: users(:ada).id,
+    #         image_url: "test URL"
+    #       }
+    #     }
+    #   }.must_change "Product.count", 1
+    #
+    #   must_respond_with :redirect
+    #   must_redirect_to products_path
+    # end
+
+  #   it "renders bad_request and does not update the DB for bogus data" do
+  #     proc {
+  #       post works_path, params: {
+  #         work: {
+  #           title: "",
+  #           creator: "An author",
+  #           category: "Book",
+  #           publication_year: 1984
+  #         }
+  #       }
+  #     }.must_change "Work.count", 0
+  #
+  #     Work.count.must_equal 4
+  #     must_respond_with :bad_request
+  #   end
   end
 
-  it "should get show" do
-    get products_show_url
-    value(response).must_be :success?
-  end
 
-  it "should get edit" do
-    get products_edit_url
-    value(response).must_be :success?
-  end
 
-  it "should get update" do
-    get products_update_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get products_destroy_url
-    value(response).must_be :success?
-  end
-
+    it "should get edit" do
+      get edit_product_path(products(:cat))
+      must_respond_with :success
+    end
 end
