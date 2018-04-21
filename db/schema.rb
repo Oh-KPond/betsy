72a73cf8c7c0ed07fb6e60c3558a53dd4f1769b1
Brittany Jones <brittany.r.jones@hotmail.com>
2018-04-20 21:14:53 -0700

    added product id column to review table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421040238) do
+ActiveRecord::Schema.define(version: 20180421040743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180421040238) do
     t.datetime "updated_at", null: false
     t.string "text_review"
     t.integer "product_id"
+    t.bigint "user_id"
   end
 
   create_table "users", force: :cascade do |t|
