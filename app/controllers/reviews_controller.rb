422ad5bfaecc4e1231d14c037f992687d367ec5d
Lily Sky <lilyxsky@gmail.com>
2018-04-23 11:06:03 -0700

    nested review route; confirmed review-product relationship good; product show has button to leave review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
 
   before_action :current_or_guest_user
-  before_action :find_product, only: [:new]
+  before_action :find_product,
 
   def new
     if session[:user_id] == @product.user_id
@@ -15,13 +15,15 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = Review.new(
+    @product = Product.find_by(id: params[:product_id])
+    @review = @product.reviews.new(
       review_params
     )
-    @product = Product.find_by(id: review_params[:product_id])
-    if save_and_flash(@review, name: "")
-      redirect_to product_path(review_params[:product_id])
-      return
+    if @review.save
+      redirect_to product_path(@product.id)
+    # save_and_flash(@review, name: "")
+    #   redirect_to product_path(review_params[:product_id])
+      # return
     else
       render :new, status: :bad_request
     end
