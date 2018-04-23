b8d07bb54360b3831fdf07d0bbe5f600de94cd3c
Lily Sky <lilyxsky@gmail.com>
2018-04-22 18:00:54 -0700

    tests pass; categories can be created, but assignment when products created isn't working yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180421055653) do
-=======
+
 ActiveRecord::Schema.define(version: 20180421194837) do
->>>>>>> feature_updating-database
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
