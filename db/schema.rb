97c2a08e072dbb32538ba05f85965386e555aeee
Steffany Brown <steffannbrown@gmail.com>
2018-04-22 17:14:03 -0700

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419221427) do
+ActiveRecord::Schema.define(version: 20180423000136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180419221427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name_on_card"
+    t.integer "user_id"
   end
 
   create_table "orders_products", id: false, force: :cascade do |t|
