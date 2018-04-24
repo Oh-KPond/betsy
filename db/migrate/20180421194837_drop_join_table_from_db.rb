c8d3ee5c7ff17135f3115a13a725206991a91623
Kate Pond <kate@thepondsedge.com>
2018-04-24 09:07:04 -0700

    removing migrations that were causing problems for people on our team

--- db/migrate/20180421194837_drop_join_table_from_db.rb
+++
@@ -1,9 +0,0 @@
-class DropJoinTableFromDb < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :products_categories do |t|
-      t.integer "quantity"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
-  end
-end
