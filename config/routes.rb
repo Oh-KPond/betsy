c8a781a5f121f52f658f3876b287efcf78eecd81
Steffany Brown <steffannbrown@gmail.com>
2018-04-25 17:44:14 -0700

    updated routes so logout should work again

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index', as: 'root'
 
-  delete '/login', to: 'sessions#destroy', as: 'logout'
+
 
 
   resources :users
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 
   get "/auth/github", as: 'github_login'
   get "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
-  get 'sessions/create'
-  delete "/auth/github/callback", as: 'github_logout'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  # delete "/auth/github/callback", to: "sessions#destroy", as: 'logout'
 
   resources :users do
     resources :products, only: [:new, :create, :edit, :update]
