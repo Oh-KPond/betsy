28275b2c427e52a74b158fb2e500cf3f7a1b61ee
Lily Sky <lilyxsky@gmail.com>
2018-04-26 13:35:49 -0700

    revenue amounts tally and display correctly

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,27 +2,6 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
 
-  # def self.by_merchant_items(id)
-  #
-  #   merchandise = Product.where(user_id: id)
-  #   # finding products that belong to the logged-in user
-  #
-  #   selected_items = []
-  #   merchandise.each do |merch|
-  #     selected_items << OrderItem.where(product_id: merch.id)
-  #   end
-  #   # loop through the products that are owned by logged in user and see if they match any of the products in any of the orders
-  #
-  #   selected_orders = []
-  #   selected_items.each do |join_item|
-  #     selected_orders << Order.where(order_id: join_item.orders)
-  #   end
-  #   # loop through orderitems that belong to logged in user to find orders that have those items in them.
-  #
-  #   return selected_orders
-  #
-  # end
-
   def self.merchant_products(id)
     Product.where(user_id: id)
   end
@@ -40,8 +19,6 @@ class Order < ApplicationRecord
     return orders.uniq
   end
 
-
-
  def get_total
    total = 0
    self.order_items.each do |item|
@@ -55,6 +32,3 @@ class Order < ApplicationRecord
 
 
 end
-
-
-# I want an array of orders_ids
